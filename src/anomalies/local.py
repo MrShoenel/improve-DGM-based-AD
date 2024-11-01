eb8907da2a2d1918dd0da3e38c383151b8c9e359
Sebastian Hönel <sebastian.honel@lnu.se>
2024-11-01 12:00:07 +0100

    Make a comment with next todos

--- src/anomalies/local.py
+++ src/anomalies/local.py
@@ -2,3 +2,8 @@
 'Local' anomalies are those that use a nominal image and add a spatially local
 anomaly, i.e., one or more regions that are somehow artifically flawed.
 """
+
+
+# TODO: We should implement a wrapper that can take a directory to textures,
+# TODO: as well as other parameters and produce local anomalies using the
+# TODO: Perlin generator.
