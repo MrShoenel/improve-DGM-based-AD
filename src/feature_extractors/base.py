35ce8a05355a1168f13eee88da24da152015d010
Sebastian Hönel <sebastian.honel@lnu.se>
2024-11-04 14:37:27 +0100

    Make FeatureExtractor inherit from nn.Module

--- src/feature_extractors/base.py
+++ src/feature_extractors/base.py
@@ -8,17 +8,18 @@ from typing import Iterable
 from numpy import ndarray, save, transpose
 from pandas import DataFrame
 from typing import Self
-from torch import Tensor
+from torch import Tensor, nn
 from re import compile, IGNORECASE
 
 
 
-class FeatureExtractor():
+class FeatureExtractor(nn.Module):
     """
     This is the mother of all (file-based) feature extractors. It is meant to provide
     common functionality, as well as a common interface for its sub-classes.
     """
-    def __init__(self, in_folder: Path, out_folder: Path|None=None) -> None:
+    def __init__(self, in_folder: Path, out_folder: Path|None=None, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
         assert isinstance(in_folder, Path) and (isinstance(out_folder, Path) or out_folder is None)
         self.in_folder = in_folder
         self.out_folder = out_folder
